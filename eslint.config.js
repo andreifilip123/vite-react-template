5574bb5e9221258ea9fe85c00d723406c65f009a
Andrei Filip <andreifilip123@gmail.com>
2023-09-15 10:15:53 +0300

    Install plugin perfectionist

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import js from "@eslint/js";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import perfectionist from "eslint-plugin-perfectionist";
+import perfectionistNatural from "eslint-plugin-perfectionist/configs/recommended-natural";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
@@ -16,6 +18,7 @@ export default [
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "@typescript-eslint": tsPlugin,
+      perfectionist,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -24,4 +27,5 @@ export default [
       "react-refresh/only-export-components": "warn",
     },
   },
+  perfectionistNatural,
 ];
